65a605e69c5f7f4eb68731e4bfe87db3125a939e
Juank Nolasco <jnolasco@Juanks-MacBook-Air.local>
2025-09-28 18:55:03 -0600

    fix: set Vite base for Guthub pages subpath

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/HCIA_Storage_Mock_Test/',   // 👈 subruta de Pages
   plugins: [react()],
-  server: {
-    hmr: { overlay: false }
-  }
+  server: { hmr: { overlay: false } }
 })
